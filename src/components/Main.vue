f8673fe7476ec41c09c533eff02a1a010898ffab
Dilara Gözübüyük <dilara.gozubuyuk@trendyol.com>
2020-12-02 21:27:16 +0300

    Install and configure Sentry

---
+++ src/components/Main.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="main">
+    test
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+
+  mounted() {
+    this.sendError();
+  },
+
+  methods: {
+    sendError() {
+      setTimeout(() => {
+        throw new Error('test');
+      }, 10000) 
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import * as Sentry from "@sentry/browser";
+import { Integrations } from "@sentry/tracing";
+
+Sentry.init({
+  Vue,
+  dsn: "",
+  integrations: [
+    new Integrations.BrowserTracing(),
+  ],
+  release: "%VUE_APP_RELEASE_VERSION%",
+});
+
 Vue.config.productionTip = false
 
 new Vue({
